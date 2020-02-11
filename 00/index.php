bfb8b3890c8224e9ee97d04d00733c1b6bd00dcc
Àlex Castelló <ac.desenvolupador@gmail.com>
2020-02-11 12:49:29 +0100

    E

---
+++ 00/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="ca">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>TITOL</title>
+  <meta name="keywords" content="">
+  <link rel="stylesheet" href="../style.css">
+
+</head>
+<body>
+  <h1>TITOL</h1>
+  <p>Estic seguint la llista de funcions de W3SCHOOLS que es troba al següent <a href="https://www.w3schools.com/php/php_ref_string.asp">enllaç</a>. Poso les que em semblen més interessants.</p>
+
+  <p>Veurem les funcions següents:</p>
+  <ul>
+   <li><strong class="resalt"></strong></li>
+  </ul>
+
+    <hr>
+
+
+    <h2><span class="resalt"></span>:<br></h2>
+    <p><strong>Exemple</strong>:</p>
+    <pre></pre>
+    <p><strong>Resultat</strong>:</p>
+    <p><?php  ?></p>
+    <hr>
+
+
+
+    <h2><span class="resalt"></span>:<br></h2>
+    <p><strong>Exemple</strong>:</p>
+    <pre></pre>
+    <p><strong>Resultat</strong>:</p>
+    <p><?php  ?></p>
+    <hr>
+
+
+
+    <h2><span class="resalt"></span>:<br></h2>
+    <p><strong>Exemple</strong>:</p>
+    <pre></pre>
+    <p><strong>Resultat</strong>:</p>
+    <p><?php  ?></p>
+    <hr>
+
+
+
+    <h2><span class="resalt"></span>:<br></h2>
+    <p><strong>Exemple</strong>:</p>
+    <pre></pre>
+    <p><strong>Resultat</strong>:</p>
+    <p><?php  ?></p>
+    <hr>
+
+
+
+    <h2><span class="resalt"></span>:<br></h2>
+    <p><strong>Exemple</strong>:</p>
+    <pre></pre>
+    <p><strong>Resultat</strong>:</p>
+    <p><?php  ?></p>
+    <hr>
+
+
+
+    <h2><span class="resalt"></span>:<br></h2>
+    <p><strong>Exemple</strong>:</p>
+    <pre></pre>
+    <p><strong>Resultat</strong>:</p>
+    <p><?php  ?></p>
+    <hr> 
+
+</body>
+</html>
